fd144c6d2c1b1eef5b208df446b94c1eba0bb50a
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-10-07 20:38:55 -0700

    BUILD

--- build/index.js
+++ build/index.js
@@ -1507,18 +1507,19 @@ defineModule(module, CafRepl = (function() {
                   } else {
                     log(("  showing: " + finalOut.length + "/" + lastOutput.length + " characters").gray);
                   }
-                  log("  show all: .last".gray);
-                  log("  result available at: global.$last".gray);
+                  log("  show all:                      .last".gray);
+                  log("  result available at:           $last".gray);
                 }
                 if (wasResolved || wasRejected) {
                   if (!wasRejected) {
-                    log("  resolved value available at: global.$lastResolved");
+                    log("  resolved value available at:   $lastResolved");
                     (_this.replEval("global", context, filename)).$lastResolved = toLog;
                   } else {
-                    log("  rejected value available at: global.$lastRejected");
+                    log("  rejected value available at:   $lastRejected");
                     (_this.replEval("global", context, filename)).$lastRejected = toLog;
                   }
-                  log("  promise available at: global.$last");
+                  (_this.replEval("global", context, filename)).$lastPromise = result;
+                  log("  promise available at:          $lastPromise");
                   return _this.cafRepl.displayPrompt();
                 }
               });
@@ -1747,7 +1748,7 @@ module.exports = JavaScript = (function(superClass) {
 /* 25 */
 /***/ (function(module, exports) {
 
-module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","bin":{"caf":"./caf"},"dependencies":{"art-build-configurator":"*","art-class-system":"*","art-config":"*","art-standard-lib":"*","art-testbench":"*","bluebird":"^3.5.0","caffeine-eight":"*","caffeine-script":"*","caffeine-script-runtime":"*","cardinal":"^1.0.0","case-sensitive-paths-webpack-plugin":"^2.1.1","chai":"^4.0.1","chalk":"^1.1.3","coffee-loader":"^0.7.3","coffee-script":"^1.12.6","colors":"^1.1.2","commander":"^2.9.0","css-loader":"^0.28.4","dateformat":"^2.0.0","detect-node":"^2.0.3","fs-extra":"^3.0.0","glob":"^7.0.3","glob-promise":"^3.1.0","json-loader":"^0.5.4","mocha":"^3.4.2","neptune-namespaces":"*","prettier":"^0.18.0","script-loader":"^0.7.0","style-loader":"^0.18.1","webpack":"^2.6.1","webpack-dev-server":"^2.4.5","webpack-merge":"^4.1.0","webpack-node-externals":"^1.6.0"},"description":"Select, configure and extend your to-JavaScript compiler, with arbitrary code, on a per file bases from within the file.","license":"ISC","name":"caffeine-mc","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register","testInBrowser":"webpack-dev-server --progress"},"version":"2.5.0"}
+module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","bin":{"caf":"./caf"},"dependencies":{"art-build-configurator":"*","art-class-system":"*","art-config":"*","art-standard-lib":"*","art-testbench":"*","bluebird":"^3.5.0","caffeine-eight":"*","caffeine-script":"*","caffeine-script-runtime":"*","cardinal":"^1.0.0","case-sensitive-paths-webpack-plugin":"^2.1.1","chai":"^4.0.1","chalk":"^1.1.3","coffee-loader":"^0.7.3","coffee-script":"^1.12.6","colors":"^1.1.2","commander":"^2.9.0","css-loader":"^0.28.4","dateformat":"^2.0.0","detect-node":"^2.0.3","fs-extra":"^3.0.0","glob":"^7.0.3","glob-promise":"^3.1.0","json-loader":"^0.5.4","mocha":"^3.4.2","neptune-namespaces":"*","prettier":"^0.18.0","script-loader":"^0.7.0","style-loader":"^0.18.1","webpack":"^2.6.1","webpack-dev-server":"^2.4.5","webpack-merge":"^4.1.0","webpack-node-externals":"^1.6.0"},"description":"Select, configure and extend your to-JavaScript compiler, with arbitrary code, on a per file bases from within the file.","license":"ISC","name":"caffeine-mc","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register","testInBrowser":"webpack-dev-server --progress"},"version":"2.5.1"}
 
 /***/ }),
 /* 26 */
